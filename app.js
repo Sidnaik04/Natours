bd9112f0c38aa2226889b7924d36e2faec5f86bf
Sidnaik04 <naiksidhant04@gmail.com>
2025-07-05 16:53:31 +0530

    add security features

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,19 +12,52 @@ const globalErrorHandler = require('./controllers/errorControllers');
 
 const app = express();
 
-// MIDDLEWARE
+// GLOBAL MIDDLEWARE
+// Set security HTTP Header
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //inbuilt middleware
 }
 
-app.use(express.json()); //inbuilt middleware
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100, //max 100 request from same IP
+  windowMs: 60 * 60 * 1000, //for 1 hr
+  message: 'Too many request from this IP, Please try again after 1 hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //inbuilt middleware
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'price',
+      'difficulty',
+      'ratingsAverage',
+      'ratingQuantity',
+    ],
+  })
+);
 
 /*app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });*/
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
@@ -39,8 +76,7 @@ app.post('/', (req, res) => {
 });
 */
 
-//ROUTES
-
+//MAIN ROUTES
 app.use('/api/v1/tours', tourRouter); //mounting routers
 app.use('/api/v1/users', userRouter); //mounting routers
 
--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -17,6 +17,21 @@ const signToken = (id) => {
 const sendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ), //90D
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // define and send cookie
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output/postman
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'Success',
     token,
